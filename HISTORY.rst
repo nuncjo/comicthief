4a1bde9844d49cb17ef5232867805f6bce7dc7fa
Nuncjo <zoreander@gmail.com>
2017-02-08 21:14:34 +0100

    Package improvements

---
+++ HISTORY.rst
@@ -0,0 +1,10 @@
+.. :changelog:
+
+Release History
+---------------
+
+0.2.0 (2017-02-08)
++++++++++++++++++++
+
+- First release
+
--- setup.py
+++ setup.py
@@ -3,22 +3,31 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='ComicThief',
-    version='0.1.7',
+    version='0.1.9',
     description='Comic Books Scraper. Easy download comics and make .cbz files.',
-    long_description=readme,
+    long_description='',
     author='Nuncjo',
     author_email='zoreander@gmail.com',
     url='https://github.com/nuncjo/comicthief',
-    license=license,
+    license='MIT',
     packages=find_packages(exclude=('tests', 'docs')),
-    package_data={'ComicThief': ['*.ini']},
-    include_package_data=True
+    package_data={'': ['*.*']},
+    include_package_data=True,
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ),
+    platforms=["Windows", "MacOS", "Unix"],
+    install_requires=['requests', 'tqdm', 'lxml']
 )
